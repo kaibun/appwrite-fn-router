436d5188db2f1606e3942287e312b7cdf8a5662d
Jean-Denis Vauguet <jd@vauguet.fr>
2025-08-04 13:39:29 +0200

    Introducting an Appwrite-aware wrapper to Itty Router

---
+++ README.md
@@ -0,0 +1,49 @@
+# Appwrite Function Router
+
+This library is a wrapper for [Itty’s `AutoRouter`](https://itty.dev/itty-router/concepts), tailored to the constraints of the Appwrite’s FaaS implementation.
+
+## Usage
+
+```ts
+import type { Context, JSONObject, ResponseObject } from "appwrite-fn-router"
+import { handleRequest } from "appwrite-fn-router";
+import { myRouteHandler } from "./routes";
+
+// Optionally define a custom JSON response schema:
+type MyJSONResponse = {
+  status: 'success' | 'error';
+  message: string;
+  error?: string;
+} & JSONObject;
+
+// This async function is your regular Appwrite’s mandatory function handler:
+export default async (context: Context) => {
+  // One may leverage Appwrite’s provided context properties if need be:
+  // const { req, res, log, error } = context;
+
+  // Those properties would be accessible from within the routes handlers thanks
+  // to the JavaScript closure, but one may import handlers from a file, so
+  // the best practice here is to  pass that context alongside your routes
+  // (defined using Itty’s API): the context is made available to the route
+  // handlers):
+  handleRequest(context, (router) => {
+    // Here’s the fuction’s context back again:
+    router.get('/hello', (req, res, _log, _error) => {
+      return res.json({
+        status: 'success',
+        message: 'Hello, world!',
+      }) satisfies ResponseObject<MyJSONResponse>;
+    });
+
+    router.post('/data', async (req, res, _log, _error) => {
+      const data = await req.bodyJson;
+      return res.json({
+        received: data,
+      });
+    });
+
+    // Despite being defined outside the closure, myRouteHandler still has
+    // access to the function’s context, through it’s params. How handy!
+    router.get("/mystery", myRouteHandler);
+  });
+```
---
+++ src/main.ts
@@ -0,0 +1,119 @@
+import { AutoRouter, IRequestStrict } from 'itty-router';
+import type {
+  Context as AppwriteContext,
+  JSONObject,
+  ResponseObject as AppwriteResponseObject,
+  Request as AppwriteRequest,
+  Response as AppwriteResponse,
+  DefaultLogger,
+  ErrorLogger,
+} from './env.d.ts';
+
+type RouterJSONResponse = {
+  status: 'success' | 'error';
+  message: string;
+  error?: string;
+} & JSONObject;
+
+export type Options = {
+  globals?: boolean;
+  env?: boolean;
+  log?: boolean;
+  onError?: (err: unknown) => void;
+};
+
+const $ = globalThis;
+
+// Creating an AutoRouter instance, adjusting types to match the Appwrite context
+export function createRouter() {
+  return AutoRouter<
+    AppwriteRequest & IRequestStrict,
+    [AppwriteResponse, DefaultLogger, ErrorLogger] & any[],
+    AppwriteResponseObject
+  >();
+}
+
+// Exporting a function to run the router with Appwrite's context
+export async function runRouter(
+  router: ReturnType<typeof createRouter>,
+  { req, res, log, error }: AppwriteContext
+) {
+  // Passing along the request and misc. objects from the Appwrite context
+  return await router.fetch(req, { res, log, error });
+}
+
+export async function handleRequest(
+  { req, res, log, error }: AppwriteContext,
+  // Accepting a function that receives the router instance, so the end-user
+  // may define their own routes, customize that router’s behavior, etc.
+  withRouter: (router: ReturnType<typeof createRouter>) => void,
+  options: Options = { globals: true, env: true, log: true }
+) {
+  options.log && log('[router] Function is starting...');
+  try {
+    if (options.globals) {
+      globalThis.log = log;
+      globalThis.error = error;
+    }
+    if (options.env) {
+      process.env.APPWRITE_FUNCTION_API_KEY =
+        req.headers['x-appwrite-key'] || '';
+    }
+    // console.log(JSON.stringify(process.env, null, 2));
+    const router = createRouter();
+    withRouter(router);
+    return runRouter(router, { req, res, log, error });
+  } catch (err) {
+    // TODO: support reporting to a monitoring service
+    options.log &&
+      error(
+        `[router] Function has failed: ${err instanceof Error ? err.stack : String(err)}`
+      );
+    const message = err instanceof Error ? err.message : String(err);
+    if (options.onError) {
+      return options.onError(err);
+    }
+    if (
+      ['/json', '/ld+json'].some((type) =>
+        req.headers['content-type']?.endsWith(type)
+      )
+    ) {
+      return res.json(
+        {
+          status: 'error',
+          message,
+          // TODO: ? don’t expose "cause" error messages in production ?
+          error:
+            err instanceof Error && err.cause instanceof Error
+              ? err.cause.message
+              : 'Reason unknown',
+        } satisfies RouterJSONResponse,
+        500
+      );
+    }
+    return res.text(message, 500);
+  }
+}
+
+/**
+ * Examples
+ */
+export default async (context: AppwriteContext) => {
+  // const { req, res, log, error } = context;
+
+  handleRequest(context, (router) => {
+    router.get('/hello', (req, res, _log, _error) => {
+      return res.json({
+        status: 'success',
+        message: 'Hello, world!',
+      }) satisfies AppwriteResponseObject<MyJSONResponse>;
+    });
+
+    router.post('/mirror', async (req, res, _log, _error) => {
+      const data = await req.bodyJson;
+      return res.json({
+        received: data,
+      });
+    });
+  });
+};
